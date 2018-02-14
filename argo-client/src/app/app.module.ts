cf83d481961f91915cd2985f35d4596c1038909e
sharp.coding <sharp.coding@gmail.com>
2018-02-14 19:41:51 +0100

    initial commit by tutorial

--- argo-client/src/app/app.module.ts
+++ argo-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { NvD3Module } from 'ng2-nvd3';
+import { StoreModule } from '@ngrx/store';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToasterService, ToasterModule } from 'angular2-toaster';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { ProjectThumbnailComponent } from './components/project-thumbnail';
 import { ProjectScreen } from './screens/project';
 import { PredictionsComponent } from './screens/project/components/predictions';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { counterReducer } from './reducers/counter';
 
 @NgModule({
   declarations: [
@@ -49,7 +51,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     BrowserAnimationsModule,
     NvD3Module,
     NgbModule.forRoot(),
-    ToasterModule
+    ToasterModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [
     ToasterService,
